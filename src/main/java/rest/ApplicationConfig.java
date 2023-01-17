6306f3128ee1d286fdcf6e87858b258377538300
michaelxuw <michaelxuwrelated@gmail.com>
2023-01-18 00:34:48 +0100

    add resources to applicationConfig

--- src/main/java/rest/ApplicationConfig.java
+++ src/main/java/rest/ApplicationConfig.java
@@ -26,6 +26,8 @@ public class ApplicationConfig extends Application {
         resources.add(errorhandling.GenericExceptionMapper.class);
         resources.add(org.glassfish.jersey.server.wadl.internal.WadlResource.class);
         resources.add(rest.FestivalResource.class);
+        resources.add(rest.GuestResource.class);
+        resources.add(rest.ShowResource.class);
         resources.add(security.JWTAuthenticationFilter.class);
         resources.add(security.LoginEndpoint.class);
         resources.add(security.RolesAllowedFilter.class);
